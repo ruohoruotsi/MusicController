42f887ee6fb9c676a1225dc092e438b246177f1e
ruohoruotsi <ruoho.ruotsi@gmail.com>
2013-08-09 23:37:26 -0700

    Beef up the minimal provider with some proper delegate and datasource methods. Work in progress

--- Demo/DPMusicControllerDemoApp/BeamMinimalExampleProvider.m
+++ Demo/DPMusicControllerDemoApp/BeamMinimalExampleProvider.m
@@ -8,29 +8,83 @@
 //
 
 #import "BeamMinimalExampleProvider.h"
+#import "DPMusicController.h"
 
 @implementation BeamMinimalExampleProvider
 
--(NSString*)musicPlayer:(BeamMusicPlayerViewController *)player albumForTrack:(NSUInteger)trackNumber {
-    return @"Example Album that is very long and therefore needs scrolling";
+#pragma mark Minimal Datasource Methods
+
+-(NSString*)musicPlayer:(BeamMusicPlayerViewController *)player titleForTrack:(NSUInteger)trackNumber {
+
+    NSString* retVal = nil;
+    DPMusicController* controller = [DPMusicController sharedController];
+    if([[controller queue] count] != 0) {
+        
+        retVal = [controller currentSong].title;
+    }
+    return retVal;
 }
 
 -(NSString*)musicPlayer:(BeamMusicPlayerViewController *)player artistForTrack:(NSUInteger)trackNumber {
-    return @"Cim Tooks and the Pineapples";
+    
+    NSString* retVal = nil;
+    DPMusicController* controller = [DPMusicController sharedController];
+    if([[controller queue] count] != 0) {
+        
+        retVal = [controller currentSong].artistName;
+    }
+    return retVal;
 }
 
--(NSString*)musicPlayer:(BeamMusicPlayerViewController *)player titleForTrack:(NSUInteger)trackNumber {
-    return @"If the Foo sings bar, it makes me wanna baz.";
+
+-(NSString*)musicPlayer:(BeamMusicPlayerViewController *)player albumForTrack:(NSUInteger)trackNumber {
+
+    NSString* retVal = nil;
+    DPMusicController* controller = [DPMusicController sharedController];
+    if([[controller queue] count] != 0) {
+        
+        retVal = [controller currentSong].albumTitle;
+    }
+    return retVal;
 }
 
 -(CGFloat)musicPlayer:(BeamMusicPlayerViewController *)player lengthForTrack:(NSUInteger)trackNumber {
-    return 125;
+   
+    CGFloat retVal = 0;
+    DPMusicController* controller = [DPMusicController sharedController];
+    if([[controller queue] count] != 0) {
+        
+        retVal = [[DPMusicController sharedController] duration];
+    }
+    return retVal;
+}
+
+#pragma mark optional
+-(CGFloat)volumeForMusicPlayer:(BeamMusicPlayerViewController*)player {
+    return [DPMusicController sharedController].player.volume;
 }
 
 -(NSInteger)numberOfTracksInPlayer:(BeamMusicPlayerViewController *)player {
-    return 3;
+
+    return [DPMusicController sharedController].queue.count;
 }
 
+
+-(void)musicPlayer:(BeamMusicPlayerViewController *)player artworkForTrack:(NSUInteger)trackNumber receivingBlock:(BeamMusicPlayerReceivingBlock)receivingBlock {
+    
+    DPMusicController* controller = [DPMusicController sharedController];
+
+    if([[controller queue] count] != 0) {
+        UIImage* image = [[controller currentSong] getRepresentativeImageForSize:player.preferredSizeForCoverArt];
+        
+        if ( image ){
+            receivingBlock(image, nil);
+        } else {
+            receivingBlock(nil,nil);
+        }
+    }
+}
+/* ALT
 -(void)musicPlayer:(BeamMusicPlayerViewController *)player artworkForTrack:(NSUInteger)trackNumber receivingBlock:(BeamMusicPlayerReceivingBlock)receivingBlock {
     NSString* url = @"http://a3.mzstatic.com/us/r1000/045/Features/7f/50/ee/dj.zygromnm.600x600-75.jpg";
     
@@ -40,10 +94,90 @@
         UIImage* image = [UIImage imageWithData:urlData];
         receivingBlock(image,nil);
     });
+} */
+
+// NEW BEAM ISH
+// -(CGFloat)musicPlayer:(BeamMusicPlayerViewController*)player currentPositionForTrack:(NSUInteger)trackNumber {
+//    return player.currentPlaybackPosition + 1.0f;
+// }
+
+
+#pragma mark Delegate Methods ( Used to control the music player )
+
+-(NSInteger)musicPlayer:(BeamMusicPlayerViewController *)player didChangeTrack:(NSUInteger)track {
+    /*
+    if(self.mediaItems) {
+        [self.musicPlayer setNowPlayingItem:[self mediaItemAtIndex:track]];
+    } else {
+        int delta = track - self.musicPlayer.indexOfNowPlayingItem;
+        if(delta > 0)
+            [self.musicPlayer skipToNextItem];
+        if(delta == 0)
+            [self.musicPlayer skipToBeginning];
+        if(delta < 0)
+            [self.musicPlayer skipToPreviousItem];
+    }
+    return self.musicPlayer.indexOfNowPlayingItem; */
 }
 
--(CGFloat)musicPlayer:(BeamMusicPlayerViewController*)player currentPositionForTrack:(NSUInteger)trackNumber {
-    return player.currentPlaybackPosition + 1.0f;
+-(void)musicPlayerDidStartPlaying:(BeamMusicPlayerViewController *)player {
+    if (![[DPMusicController sharedController] isPlaying]) {
+        [[DPMusicController sharedController] play:nil];
+    }
+}
+
+-(void)musicPlayerDidStopPlaying:(BeamMusicPlayerViewController *)player {
+    if ([[DPMusicController sharedController] isPlaying]) {
+        [[DPMusicController sharedController] pause:nil];
+    }
+}
+
+-(void)musicPlayer:(BeamMusicPlayerViewController *)player didChangeVolume:(CGFloat)volume {
+    [[DPMusicController sharedController] setVolume:volume];
+}
+
+-(void)musicPlayer:(BeamMusicPlayerViewController *)player didSeekToPosition:(CGFloat)position {
+    
+    //[self.musicPlayer setCurrentPlaybackTime:position];
+    //[[DPMusicController sharedController] setTrackPosition:position ];
+
+    [[DPMusicController sharedController] seekToTime:position ];
+
+    
+  // self.remainingLabel.text = [[[DPMusicController sharedController].player ] remainingTimeString];
+  // self.elapsedLabel.text = [[DPMusicController sharedController] elapsedTimeString];
+  // self.scrubSlider.value = [[DPMusicController sharedController] trackPosition] / [[DPMusicController sharedController] duration];
+}
+
+
+-(BOOL)musicPlayerShouldStartPlaying:(BeamMusicPlayerViewController*)player {
+    // If return value is NO, the player won't start playing.
+    // YES, tells the player to starts. Default is YES.
+    return YES;
+}
+
+-(void)musicPlayerDidStopPlayingLastTrack:(BeamMusicPlayerViewController*)player {
+    //  Called after the player stopped playing the last track.
+}
+
+-(BOOL)musicPlayerShouldStopPlaying:(BeamMusicPlayerViewController*)player {
+    // By returning NO here, the delegate may prevent the player from stopping the playback
+    return YES;
+}
+
+-(BOOL)musicPlayer:(BeamMusicPlayerViewController*)player shouldChangeTrack:(NSUInteger)track {
+    // return YES if the track can be changed, NO if not. Default YES.
+    return YES;
+}
+
+-(void)musicPlayer:(BeamMusicPlayerViewController*)player didChangeShuffleState:(BOOL)shuffling {
+    // YES indicates the player is shuffling now, i.e. randomly selecting a next track from the valid range of tracks
+    // NO means there is no shuffling.
+}
+
+-(void)musicPlayer:(BeamMusicPlayerViewController*)player didChangeRepeatMode:(MPMusicRepeatMode)repeatMode {
+    // The repeat modes are taken from MediaPlayer framework and indicate
+    // whether the player is in No Repeat, Repeat Once or Repeat All mode.
 }
 
 @end
