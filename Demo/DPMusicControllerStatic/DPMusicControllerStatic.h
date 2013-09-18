97ea75f308fa83565b93a32078e7ad3bb47ccbdb
ruohoruotsi <ruoho.ruotsi@gmail.com>
2013-09-18 00:23:28 -0700

    [BUGFIX] for #10 (https://github.com/ruohoruotsi/DPMusicController/issues/10) Make a fake static framework with embedded resources. The embedded framework was tested out in an openframeworks iOS Native example project using storyboards

---
+++ Demo/DPMusicControllerStatic/DPMusicControllerStatic.h
@@ -0,0 +1,13 @@
+//
+//  DPMusicControllerStatic.h
+//  DPMusicControllerStatic
+//
+//  Created by Orife, Iroro on 9/15/13.
+//  Copyright (c) 2013 Dan Pourhadi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DPMusicControllerStatic : NSObject
+
+@end
---
+++ Demo/DPMusicControllerStatic/DPMusicControllerStatic.m
@@ -0,0 +1,13 @@
+//
+//  DPMusicControllerStatic.m
+//  DPMusicControllerStatic
+//
+//  Created by Orife, Iroro on 9/15/13.
+//  Copyright (c) 2013 Dan Pourhadi. All rights reserved.
+//
+
+#import "DPMusicControllerStatic.h"
+
+@implementation DPMusicControllerStatic
+
+@end
